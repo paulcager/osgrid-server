6eda3c5d98421da2661de453174037ac6d4b6a57
Paul Cager <paul.cager@gmail.com>
2020-12-07 18:24:59 +0000

    URL paths

--- index.js
+++ index.js
@@ -1,32 +1,26 @@
+// Convert between Ordnance Survey grid reference and WGS-84 lat/lon.
+// See https://www.movable-type.co.uk/scripts/latlong-os-gridref.html
+
 import OsGridRef from 'geodesy/osgridref.js';
 import { LatLon } from 'geodesy/osgridref.js';
 const url = require('url');
-
-
 const express = require('express');
+
 const app = express();
 
-
-// Convert between Ordnance Survey grid reference and WGS-84 lat/lon.
-// See https://www.movable-type.co.uk/scripts/latlong-os-gridref.html
-//
-// Usage: http://server/?gridRef=osgb-grid-ref or ://server/?latlon=lat,lon
-app.get('/', (request, response) => {
-    var urlParts = url.parse(request.url, true);
-    var parameters = urlParts.query;
-
-    if (parameters.gridRef) {
-        const gridref = OsGridRef.parse(parameters.gridRef);
+// Usage: http://server/gridref=osgb-grid-ref
+app.get('/gridref/:ref', (request, response) => {
+        const gridref = OsGridRef.parse(request.params.ref);
         const wgs84 = gridref.toLatLon();
         response.send(String(wgs84.lat) + "," + String(wgs84.lon));
-    } else if (parameters.latLon) {
-        const parts = parameters.latLon.split(",");
-        const wgs84 = new LatLon(parts[0], parts[1]);
-        const gridref = wgs84.toOsGrid();
-        response.send(gridref.toString());
-    } else {
-        response.send(404);
-    }
+});
+
+// Usage: http://server/latlon=lat,lon
+app.get('/latlon/:latlon', (request, response) => {
+    const parts = request.params.latlon.split(",");
+    const wgs84 = new LatLon(parts[0], parts[1]);
+    const gridref = wgs84.toOsGrid();
+    response.send(gridref.toString());
 });
 
 app.listen(9090, () => console.log('Listening on port 9090'));
